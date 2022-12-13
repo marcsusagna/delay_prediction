554646fedc5fe60d5a8b3c9932226577f4be459d
Marc Susagna <marcsusagna@gmail.com>
2022-12-13 22:58:23 +0100

    Updated train script to bring to local machine the processed data

--- train.ps1
+++ train.ps1
@@ -4,6 +4,9 @@ docker run -dit --name train_app_container delay_train_app:0.0.1
 docker exec train_app_container python data_onboarding_main.py
 docker exec train_app_container python preprocessing_main.py
 docker exec train_app_container python train_main.py $model_version
+# Bring data to the local machine
+docker cp train_app_container:/delay_train_app/data/onboarded/ data/onboarded/
+docker cp train_app_container:/delay_train_app/data/clean/ data/clean/
 docker cp train_app_container:/delay_train_app/model_registry/$model_version/trained_model.joblib model_registry/$model_version
 docker stop train_app_container
 docker rm train_app_container
